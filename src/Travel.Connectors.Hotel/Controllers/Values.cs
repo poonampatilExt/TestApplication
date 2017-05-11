c0bb40914cd000ddee808afa7008429f0c85381e
poonampatilExt <poonam.patil@extetnia.com>
2017-05-11 16:38:33 +0530

    yu om

--- src/Travel.Connectors.Hotel/Controllers/Values.cs
+++ src/Travel.Connectors.Hotel/Controllers/Values.cs
@@ -10,7 +10,7 @@ namespace Travel.Connectors.Hotel.Controllers
         [HttpGet]
         public IEnumerable<string> Get(string test)
         {
-            return new string[] { "value1", "value--OM32 55" };
+            return new string[] { "value1", "value--OM32" };
         }
 
         // GET api/values/5
